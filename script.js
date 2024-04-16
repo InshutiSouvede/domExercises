9f38597bee66e9d7998c2272a3eef465c0e6c979
InshutiSouvede <isouvede2@gmail.com>
2024-04-16 10:08:25 +0200

    changing bg and printing details of a product when user clicks buy

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // function changeAllArticleColors(){
 //     const products = document.querySelectorAll('#products article')
 //     console.log(products)
-//     for (let article of products){
-//         console.log(article)
-//         article.classList.add("sale")
-//     }
+    // for (let article of products){
+    //     console.log(article)
+    //     article.classList.add("sale")
+    // }
 
 // }
 // changeAllArticleColors()
@@ -12,8 +12,14 @@
 function attachBuyEvents(){
     const buttons = document.querySelectorAll("#products button")
     console.log(buttons)
-    buttons.addEventListener('click',()=>{
-        
-    })
+    for (let button of buttons){
+        button.addEventListener('click',(e)=>{
+            console.log("User clicked "+e.target.nodeName)
+            console.log("With Id: ",button.parentElement.getAttribute('data-id'))
+            button.parentElement.classList.toggle('sale')
+            console.log("You are ordering: ",button.parentElement.querySelector('input').value," ",button.parentElement.querySelector('h2').textContent)
+
+        })
+    }
 }
 attachBuyEvents()
