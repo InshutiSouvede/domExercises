6acfc034bd08d6173eeae2070621c1675768c1fc
InshutiSouvede <isouvede2@gmail.com>
2024-04-16 08:47:32 +0200

    change article colors

---
+++ script.js
@@ -0,0 +1,19 @@
+// function changeAllArticleColors(){
+//     const products = document.querySelectorAll('#products article')
+//     console.log(products)
+//     for (let article of products){
+//         console.log(article)
+//         article.classList.add("sale")
+//     }
+
+// }
+// changeAllArticleColors()
+
+function attachBuyEvents(){
+    const buttons = document.querySelectorAll("#products button")
+    console.log(buttons)
+    buttons.addEventListener('click',()=>{
+        
+    })
+}
+attachBuyEvents()
