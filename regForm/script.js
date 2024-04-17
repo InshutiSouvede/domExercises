f7a52bcc0b53fa4da5e6189877110c9f54bb124e
InshutiSouvede <isouvede2@gmail.com>
2024-04-17 16:28:52 +0200

    add labels to the input fields

---
+++ regForm/script.js
@@ -0,0 +1,7 @@
+function addLables(){
+    document.getElementById('username').setAttribute('placeholder','username')
+    document.getElementById('password').setAttribute('placeholder','password')
+    document.getElementById('confirmPassword').setAttribute('placeholder','confirm password')
+
+}
+addLables()
