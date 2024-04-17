244b467ba30aaca0f76ab19edb8081e6b610462c
InshutiSouvede <isouvede2@gmail.com>
2024-04-17 14:22:33 +0200

    paragraph manupilation

--- script.js
+++ script.js
@@ -1,80 +1,104 @@
-// function changeAllArticleColors(){
-//     const products = document.querySelectorAll('#products article')
-//     console.log(products)
-// for (let article of products){
-//     console.log(article)
-//     article.classList.add("sale")
-// }
-
-// }
-// changeAllArticleColors()
-
-function attachBuyEvents() {
-    const buttons = document.querySelectorAll("#products button")
-    console.log(buttons)
-    for (let button of buttons) {
-        button.addEventListener('click', (e) => {
-            // console.log("User clicked "+e.target.nodeName)
-            // console.log("With Id: ",button.parentElement.getAttribute('data-id'))
-            // button.parentElement.classList.toggle('sale')
-            // console.log("You are ordering: ",button.parentElement.querySelector('input').value," ",button.parentElement.querySelector('h2').textContent)
-            let table = document.querySelector('table')
-            let ttrow = document.querySelector('tfoot tr').querySelectorAll('th')
-            console.log('Hello',ttrow[1])
-            let rows = document.querySelectorAll('table tr'), trigger = 0
-            for (row of rows) {
-                let cols = row.querySelectorAll('td')
-                if (cols[0] && cols[0].textContent == button.parentElement.getAttribute('data-id')) {
-                    cols[2].textContent = button.parentElement.querySelector('input').value
-
-                    let pastPrice = cols[4].textContent
-                    cols[4].textContent = parseInt(cols[2].textContent) * parseInt(cols[3].textContent)
-
-
-                    ttrow[1].textContent = parseInt(ttrow[1].textContent)+ parseInt(cols[4].textContent)-pastPrice
-                    trigger = 1
-                }
-            }
-            if (trigger == 0) {
-                const row = document.createElement('tr')
-
-                const tdId = document.createElement('td')
-                tdId.textContent = button.parentElement.getAttribute('data-id')
-
-                const tdproduct = document.createElement('td')
-                tdproduct.textContent = button.parentElement.querySelector('h2').textContent
-
-                const tdquantity = document.createElement('td')
-                tdquantity.textContent = button.parentElement.querySelector('input').value
-
-                const tdprice = document.createElement('td')
-                tdprice.textContent = button.parentElement.querySelector('.price').textContent
-
-                const tdTtl = document.createElement('td')
-                tdTtl.textContent = parseInt(tdquantity.textContent) * parseInt(tdprice.textContent)
-
-                const tdDel = document.createElement('td')
-                tdDel.textContent = 'X'
-                tdDel.addEventListener('click', (e) => {
-                    // update the total amount
-                    console.log("price to delete",e.target.parentElement.querySelectorAll('td')[4].textContent)
-                    ttrow[1].textContent = parseInt(ttrow[1].textContent) - parseInt(e.target.parentElement.querySelectorAll('td')[4].textContent)
-                    e.target.parentElement.remove()
-                })
-                ttrow[1].textContent = parseInt(ttrow[1].textContent)+ parseInt(tdTtl.textContent)
-
-
-                row.appendChild(tdId)
-                row.appendChild(tdproduct)
-                row.appendChild(tdquantity)
-                row.appendChild(tdprice)
-                row.appendChild(tdTtl)
-                row.appendChild(tdDel)
-
-                table.appendChild(row)
-            }
-
-        })
+function highlight() {
+    let paragraph = document.querySelector('p').textContent
+    let pArr = paragraph.split(' ')
+    console.log(pArr)
+    let fcontent = ''
+    for (i = 0; i < pArr.length; i++) {
+        if (pArr[i].length > 8) {
+            fcontent += ' <span style="background-color:yellow;">' + pArr[i] + "</span>"
+        } else {
+            fcontent += " " + pArr[i]
+        }
+        // console.log(fcontent)
     }
+    document.querySelector('p').innerHTML = fcontent
+    let par = document.querySelector('p')
+
+    par.insertAdjacentHTML("afterend", "<a href='https://forcemipsum.com'>Anotherr course</a>")
+
 }
-attachBuyEvents()
+// highlight()
+function splitPar() {
+    let par = document.querySelector('p').textContent
+    let content = '<p>'
+    for (i = 0; i < par.length; i++) {
+        if (par[i] == "\n") {
+            content += '</p><p>'
+            console.log("encountered next line after", par[i - 1])
+        } else {
+            content += par[i]
+        }
+    }
+    document.querySelector('p').outerHTML = content + "</p>"
+}
+// splitPar()
+function numPar() {
+    // console.log("I was called")
+    let par = document.getElementsByTagName('p')[0].textContent
+    let counter = par.split(' ').length
+    console.log(counter)
+    document.querySelector('h1').insertAdjacentHTML("afterend", '<i>(' + counter + ') words</i>')
+
+}
+// numPar()
+function replaceMark() {
+    console.log("I was called")
+    let par = document.querySelector('p').textContent
+    
+    let content = ''
+    for (i = 0; i < par.length; i++) {
+        if (par[i] == "?") {
+            content += '🤔'
+            // console.log("encountered next line after", par[i - 1])
+        } else {
+            content += par[i]
+        }
+    }
+    document.querySelector('p').textContent = content
+}
+// replaceMark()
+function allInOne(){
+    let paragraph = document.querySelector('p').textContent
+
+    let pArr = paragraph.split(' ')
+    document.querySelector('h1').insertAdjacentHTML("afterend", '<i>(' + pArr.length + ') words</i>')
+    console.log(pArr)
+    let fcontent = '<p>'
+    for (i = 0; i < pArr.length; i++) {
+        let charAdded = 0
+        // console.log(pArr[i].includes('\n')?"This includes a next line "+pArr[i]:"")
+        if (pArr[i].length > 8) {
+            fcontent += ' <span style="background-color:yellow;">' + pArr[i] + "</span>"
+            charAdded = 1
+        }
+        if(pArr[i].includes('\n') && pArr[i].includes("?") ) {
+            let n = pArr.indexOf('\n')
+            let q = pArr.indexOf('?')
+            console.log("the word with nextline ", pArr[i]," it will become ",pArr[i].slice(0,n)+'</p><p>')
+            fcontent += " "+pArr[i].slice(0,q)+pArr[i].slice(q,n)+'</p><p>'
+            charAdded = 1
+            // console.log("encountered next line after", pArr[i - 1])
+        } 
+        else if(pArr[i].endsWith('\n')){
+            fcontent += " "+pArr[i].slice(0,pArr[i].length-1)+'</p><p>'
+        }
+        else if (pArr[i].endsWith("?")) {
+            
+            // console.log("the word with ? ", pArr[i]," it will become ",pArr[i].slice(0,q)+'🤔')
+            fcontent += " "+pArr[i].slice(0,pArr[i].length-1)+'🤔'
+            charAdded = 1
+            // console.log("encountered next line after", par[i - 1])
+        } 
+    
+        if(!charAdded) {
+            fcontent += " " + pArr[i]
+        }
+        // console.log(fcontent)
+    }
+    document.querySelector('p').innerHTML = fcontent
+    let par = document.querySelector('p')
+
+    par.insertAdjacentHTML("afterend", "<a href='https://forcemipsum.com'>Anotherr course</a>")
+
+}
+// allInOne()
---
+++ stock/script.js
@@ -0,0 +1,80 @@
+// function changeAllArticleColors(){
+//     const products = document.querySelectorAll('#products article')
+//     console.log(products)
+// for (let article of products){
+//     console.log(article)
+//     article.classList.add("sale")
+// }
+
+// }
+// changeAllArticleColors()
+
+function attachBuyEvents() {
+    const buttons = document.querySelectorAll("#products button")
+    console.log(buttons)
+    for (let button of buttons) {
+        button.addEventListener('click', (e) => {
+            // console.log("User clicked "+e.target.nodeName)
+            // console.log("With Id: ",button.parentElement.getAttribute('data-id'))
+            // button.parentElement.classList.toggle('sale')
+            // console.log("You are ordering: ",button.parentElement.querySelector('input').value," ",button.parentElement.querySelector('h2').textContent)
+            let table = document.querySelector('table')
+            let ttrow = document.querySelector('tfoot tr').querySelectorAll('th')
+            console.log('Hello',ttrow[1])
+            let rows = document.querySelectorAll('table tr'), trigger = 0
+            for (row of rows) {
+                let cols = row.querySelectorAll('td')
+                if (cols[0] && cols[0].textContent == button.parentElement.getAttribute('data-id')) {
+                    cols[2].textContent = button.parentElement.querySelector('input').value
+
+                    let pastPrice = cols[4].textContent
+                    cols[4].textContent = parseInt(cols[2].textContent) * parseInt(cols[3].textContent)
+
+
+                    ttrow[1].textContent = parseInt(ttrow[1].textContent)+ parseInt(cols[4].textContent)-pastPrice
+                    trigger = 1
+                }
+            }
+            if (trigger == 0) {
+                const row = document.createElement('tr')
+
+                const tdId = document.createElement('td')
+                tdId.textContent = button.parentElement.getAttribute('data-id')
+
+                const tdproduct = document.createElement('td')
+                tdproduct.textContent = button.parentElement.querySelector('h2').textContent
+
+                const tdquantity = document.createElement('td')
+                tdquantity.textContent = button.parentElement.querySelector('input').value
+
+                const tdprice = document.createElement('td')
+                tdprice.textContent = button.parentElement.querySelector('.price').textContent
+
+                const tdTtl = document.createElement('td')
+                tdTtl.textContent = parseInt(tdquantity.textContent) * parseInt(tdprice.textContent)
+
+                const tdDel = document.createElement('td')
+                tdDel.textContent = 'X'
+                tdDel.addEventListener('click', (e) => {
+                    // update the total amount
+                    console.log("price to delete",e.target.parentElement.querySelectorAll('td')[4].textContent)
+                    ttrow[1].textContent = parseInt(ttrow[1].textContent) - parseInt(e.target.parentElement.querySelectorAll('td')[4].textContent)
+                    e.target.parentElement.remove()
+                })
+                ttrow[1].textContent = parseInt(ttrow[1].textContent)+ parseInt(tdTtl.textContent)
+
+
+                row.appendChild(tdId)
+                row.appendChild(tdproduct)
+                row.appendChild(tdquantity)
+                row.appendChild(tdprice)
+                row.appendChild(tdTtl)
+                row.appendChild(tdDel)
+
+                table.appendChild(row)
+            }
+
+        })
+    }
+}
+attachBuyEvents()
